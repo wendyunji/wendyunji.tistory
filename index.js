18d9477a207c36748e529682a728c90b9534521d
Yunji <wendyunji@gmail.com>
2025-01-29 23:47:11 +0900

    feat: cron once a day

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ let text = `# Hi there 👋
 
 ## 언어
 
-<p>
-  <img alt="" src= "https://img.shields.io/badge/JavaScript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white"/> 
-  <img alt="" src= "https://img.shields.io/badge/TypeScript-black?logo=typescript&logoColor=blue"/>
-</p>
-
 ## 📕 Latest Blog Posts
 
 `;
